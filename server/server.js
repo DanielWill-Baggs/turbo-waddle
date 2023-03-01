7735feac1955ee4430936a20605fe9253b03bd83
Daniel Baggs <danielwillbaggs@gmail.com>
2023-03-01 17:11:33 -0500

    Added Mongoose, DB, ENV Variables to the Project to connect to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,9 +1,33 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const portNumber = process.env.PORT;
 
 // Store the Express object in App Constant
 const app = express();
 
-// Listen for Requests set on Port 4000
-app.listen(4000, () => {
-  console.log("Port 4000 is listening!");
+// Middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ mssg: "Welcome to the Application" });
+});
+
+// Using Mongoose to connect to the DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // Listen for Requests set on Port from ENV
+    app.listen(portNumber, () => {
+      console.log(
+        "Port " + portNumber + " is listening! Sucessfully Connected to DB"
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
