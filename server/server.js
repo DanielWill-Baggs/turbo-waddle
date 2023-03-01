4b886830ef0bde57c11f5f826a4b69f54c16c8b5
Daniel Baggs <danielwillbaggs@gmail.com>
2023-03-01 16:17:16 -0500

    Opened Port 4000 for Express and Created Dev Script to Run the Backend

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
 const express = require("express");
+
+// Store the Express object in App Constant
+const app = express();
+
+// Listen for Requests set on Port 4000
+app.listen(4000, () => {
+  console.log("Port 4000 is listening!");
+});
