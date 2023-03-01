7deac4e0359df9ce70b54bdb7c65be508a0623fb
Daniel Baggs <danielwillbaggs@gmail.com>
2023-03-01 17:39:21 -0500

    Created the Routes,Models Sub-directory and Created Models for Video, updated POST method

--- server/routes/video-routes.js
+++ server/routes/video-routes.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const Video = require("../models/video-model");
 const router = express.Router();
 
 // Routes
+
+// GET method to return all videos in the DB
 router.get("/", (req, res) => {
-  res.json({ mssg: "Welcome to the Application" });
+  res.json({ mssg: "GET all videos" });
 });
+
+// POST method to write to the DB a Video instance
+router.post("/", async (req, res) => {
+  const { title } = req.body;
+  try {
+    const video = await Video.create({ title });
+    res.status(200).json(video);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const videoRoutes = require("./routes/video-routes");
 const portNumber = process.env.PORT;
 
 // Store the Express object in App Constant
 const app = express();
 
 // Middleware
+app.use("/api/video", videoRoutes);
+
+app.use(express.json());
+
+// Console logs the path and method
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
-});
-
-// Routes
-app.get("/", (req, res) => {
-  res.json({ mssg: "Welcome to the Application" });
 });
 
 // Using Mongoose to connect to the DB
